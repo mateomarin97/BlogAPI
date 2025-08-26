f224bb123eeb0a9f34fd510209a5bdff537e2396
mateomarin97 <mateomarin97@hotmail.com>
2025-08-26 15:33:07 -0500

    Added CORSM
    Now all origins can talk with our API

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from BlogAPI.database import engine, Base
 from BlogAPI.routers import blog, user, authentication, vote
 
@@ -7,6 +8,14 @@ app = FastAPI(
     title="Blog API",
     description="A simple API for managing blogs and users.",
     version="1.0.0"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
 )
 
 # Include routers for different API endpoints
