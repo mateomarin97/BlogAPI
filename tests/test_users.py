076b879a4a2cbc3ab32d9f7cda8637e061f78406
mateomarin97 <mateomarin97@hotmail.com>
2025-09-01 20:25:49 -0500

    Build test database
    Now the testing is performed in its own database.

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,6 +1,59 @@
 import pytest
 from fastapi.testclient import TestClient
 from BlogAPI.main import app
+from BlogAPI import schemas
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from BlogAPI.config import settings
+from BlogAPI.database import get_db, Base
+
+database_type = "PostgreSQL"
+
+if database_type == "SQLite":
+    # SQLite database URL for SQLAlchemy
+    # Can be left as it is, if no blog.db file is found it shall be created automatically
+    SQLALCHEMY_DATABASE_URL = "sqlite:///./BlogAPI/blog_test.db"
+    # Create the SQLAlchemy engine
+    engine = create_engine(
+        SQLALCHEMY_DATABASE_URL,
+        connect_args={"check_same_thread": False}  # Needed for SQLite
+    )
+elif database_type == "PostgreSQL":
+    # Postgres database URL for SQLAlchemy
+    # Here you will have to provide the correct database URL depending on your setup
+    SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}_test"
+    engine = create_engine(
+        SQLALCHEMY_DATABASE_URL
+    )
+else:
+    raise ValueError("Unsupported database type.")
+
+# Create a configured "Session" class
+TestingSessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
+Base.metadata.create_all(bind=engine)
+
+def override_get_db():
+    """
+    Dependency that provides a SQLAlchemy database session.
+
+    Yields:
+        Session: SQLAlchemy database session.
+
+    Ensures the session is closed after use.
+    """
+    db = TestingSessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+#This swaps the original get_db with the override for testing
+app.dependency_overrides[get_db] = override_get_db
 
 client = TestClient(app)
 
@@ -16,7 +69,8 @@ def test_create_delete_user():
     response = client.post("/users/", json={"name": "Test User", "email": "test@example.com", "password": "testpassword"})
     assert response.status_code == 201
     assert isinstance(response.json(), dict)
-    assert response.json().get("id") is not None
+    new_user = schemas.ShowUser.model_validate(response.json()) 
+    assert new_user.id is not None
     
     #response = client.delete("/users/")
     #assert response.status_code == 204
