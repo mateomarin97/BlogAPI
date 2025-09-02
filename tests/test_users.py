797ef04986a3753da451db1cde17884f9e5f02ad
mateomarin97 <mateomarin97@hotmail.com>
2025-09-01 21:30:13 -0500

    Logging testing done

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,6 +1,16 @@
 import pytest
 from BlogAPI import schemas
+from jose import jwt
+from BlogAPI.config import settings
 from BlogAPI.tests.database import client, session
+
+@pytest.fixture()
+def test_user(client):
+    user_data = {"name": "Nathan", "email": "mateomarin97@hotmail.com", "password": "123"}
+    response = client.post("/users/", json=user_data)
+    assert response.status_code == 201
+    user_data["id"] = response.json().get("id")
+    return user_data
 
 @pytest.mark.parametrize("user_id", [(1),(2),(3)])
 def test_read_users(user_id, client):
@@ -16,4 +26,22 @@ def test_create_user(client):
     assert isinstance(response.json(), dict)
     new_user = schemas.ShowUser.model_validate(response.json()) 
     assert new_user.id is not None
+
+def test_login(client, test_user):
+    #Notice we have /login not /login/ since we did not use prefixes when defining the route
+    #Also notice we are using form data not json.
+    response = client.post("/login", data={"username": test_user["email"], "password": test_user["password"]})
+    assert response.status_code == 200
+    assert isinstance(response.json(), dict)
+    token = schemas.Token.model_validate(response.json())
+
+    #Now we decode the token
+    payload = jwt.decode(token.access_token, settings.secret_key, algorithms=[settings.algorithm])
+    username: str = payload.get("sub")
+    if username is None:
+        raise credentials_exception
+    user_id=payload.get("id")
     
+    assert username == test_user["email"]
+    assert user_id == test_user["id"]
+    assert token.token_type == "bearer"
